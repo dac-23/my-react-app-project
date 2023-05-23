817638645d2d1eba55a60ee9f93e6ff23dd31488
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 12:00:39 +0530

    stateful variable change image.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
 function EventDemo() {
   // let counter = 100;
   let [counter, setCounter] = useState(100);
+  let [displayImage, setDisplayImage] = useState(
+    "https://picsum.photos/id/237/300/300"
+  );
 
   let likeMe = () => {
     counter = counter + 1;
@@ -19,13 +22,27 @@ function EventDemo() {
     setCounter(counter);
   };
 
+  let changeImage = () => {
+    displayImage = "https://picsum.photos/id/238/300/300";
+    // perform dom operation
+    setDisplayImage(displayImage);
+  };
+
+  let resetImage = () => {
+    displayImage = "https://picsum.photos/id/237/300/300";
+    // perform dom operation
+    setDisplayImage(displayImage);
+  };
+
   return (
     <div>
-      <h1>Like</h1>
-
-      <h1>{counter}</h1>
+      <img src={displayImage} />
+      <h1>Like {counter}</h1>
 
       <input type="button" value="Like Me" onClick={likeMe} />
+      <br />
+      <input type="button" value="Chnage Image" onClick={changeImage} />
+      <input type="button" value="Reset Image" onClick={resetImage} />
     </div>
   );
 }
