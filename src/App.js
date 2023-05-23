30528db54e82393a7079ae1b0cdfc18e8408856e
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 11:39:07 +0530

    basic destructint with stateful varaibl. e

--- src/App.js
+++ src/App.js
@@ -9,49 +9,13 @@ function App() {
 }
 
 function EventDemo() {
-  // DATA MEMBER :: Stateless or Stateful
-  // let background = "bg-primary";
-  let [background, setBackground] = useState("bg-primary");
+  // delcaring and destructing the stateful variable
+  // let title = "Hello World";
+  let [title, setTitle] = useState("Hello World");
 
-  // MEMBER FUNCN
-  let changeBackgroundPrimary = () => {
-    let newbackground = "bg-primary";
-    setBackground(newbackground);
-  };
-
-  let changeBackgroundSuccess = () => {
-    let newbackground = "bg-success";
-    setBackground(newbackground);
-  };
-
-  let changeBackgroundDanger = () => {
-    let newbackground = "bg-danger";
-    setBackground(newbackground);
-  };
-
-  // UI
   return (
     <div>
-      <h1 className={`${background} text-light p-3`}>Event DEMO</h1>
-
-      <input
-        type="button"
-        className="btn btn-primary"
-        value="PRIMARY"
-        onClick={changeBackgroundPrimary}
-      />
-      <input
-        type="button"
-        className="btn btn-success"
-        value="SUCCESS"
-        onClick={changeBackgroundSuccess}
-      />
-      <input
-        type="button"
-        className="btn btn-danger"
-        value="DANGER"
-        onClick={changeBackgroundDanger}
-      />
+      <h1>{title}</h1>
     </div>
   );
 }
