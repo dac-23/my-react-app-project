c98197b9ee2a08a198a90043c5603e7906964fc1
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 12:32:49 +0530

    working with input.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
   return (
@@ -11,23 +11,25 @@ function App() {
 // MAP OPERATOR
 // <h1>Delhi</h1>
 function EventDemo() {
+  let inputRef = useRef();
   let [list, setList] = useState(["todo1"]);
 
   // Member Funcn
   let addNewItem = () => {
-    // We should not use document object
-    let inputTag = document.querySelector("#inputid");
-    let inputValue = inputTag.value;
+    console.log(inputRef.current);
 
-    let newList = [inputValue, ...list];
-    setList(newList);
+    let newValue = inputRef.current.value;
+    setList([newValue, ...list]);
+
+    inputRef.current.value = "";
   };
 
+  // UI
   return (
     <div>
       <h1>List Demo </h1>
 
-      <input type="text" placeholder="Enter Todo" id="inputid" />
+      <input type="text" placeholder="Enter Todo" ref={inputRef} />
       <input type="button" value="Add new Item" onClick={addNewItem} />
 
       <hr />
