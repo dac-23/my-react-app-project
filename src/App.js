e16e51254df7aa8c3ac13fcd35fcbd9828fd3f5f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 11:11:36 +0530

    basic setup.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ function EventDemo() {
   return (
     <div>
       <h1 className="bg-primary text-light p-3">Event DEMO</h1>
+
+      <input type="button" className="btn btn-primary" value="PRIMARY" />
+      <input type="button" className="btn btn-success" value="SUCCESS" />
+      <input type="button" className="btn btn-danger" value="DANGER" />
+
+      <p className="m-2 fs-4 ">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        tempora at debitis reprehenderit molestiae libero. Deserunt non
+        asperiores commodi saepe, fugiat est quae temporibus ex iusto
+        perferendis consequatur ab corporis eligendi vero vel alias eaque
+        aliquid id. Iste obcaecati, adipisci reiciendis nam non suscipit eos
+        repudiandae amet! Dolorum ratione maiores quas blanditiis deserunt
+        soluta ad, odit neque quo, est amet tempora officiis error corrupti
+        facere molestias similique explicabo velit quis cum veritatis, molestiae
+        unde illo? Odio amet doloribus labore aperiam qui voluptates at optio
+        quis repudiandae fuga officiis autem, iure, nesciunt dolorem nisi
+        nostrum est magnam alias aliquid, exercitationem ut?
+      </p>
     </div>
   );
 }
