618ad950e215c6bda2cb02968952202cda4aa4c2
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 11:25:24 +0530

    setateful.

--- src/App.js
+++ src/App.js
@@ -7,27 +7,48 @@ function App() {
 }
 
 function EventDemo() {
+  // DATA MEMBER :: Stateless or Stateful
+  let background = "bg-primary";
+
+  // MEMBER FUNCN
+  let changeBackgroundPrimary = () => {
+    background = "bg-primary";
+    alert(background);
+  };
+
+  let changeBackgroundSuccess = () => {
+    background = "bg-success";
+    alert(background);
+  };
+
+  let changeBackgroundDanger = () => {
+    background = "bg-danger";
+    alert(background);
+  };
+
+  // UI
   return (
     <div>
-      <h1 className="bg-primary text-light p-3">Event DEMO</h1>
+      <h1 className={`${background} text-light p-3`}>Event DEMO</h1>
 
-      <input type="button" className="btn btn-primary" value="PRIMARY" />
-      <input type="button" className="btn btn-success" value="SUCCESS" />
-      <input type="button" className="btn btn-danger" value="DANGER" />
-
-      <p className="m-2 fs-4 ">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
-        tempora at debitis reprehenderit molestiae libero. Deserunt non
-        asperiores commodi saepe, fugiat est quae temporibus ex iusto
-        perferendis consequatur ab corporis eligendi vero vel alias eaque
-        aliquid id. Iste obcaecati, adipisci reiciendis nam non suscipit eos
-        repudiandae amet! Dolorum ratione maiores quas blanditiis deserunt
-        soluta ad, odit neque quo, est amet tempora officiis error corrupti
-        facere molestias similique explicabo velit quis cum veritatis, molestiae
-        unde illo? Odio amet doloribus labore aperiam qui voluptates at optio
-        quis repudiandae fuga officiis autem, iure, nesciunt dolorem nisi
-        nostrum est magnam alias aliquid, exercitationem ut?
-      </p>
+      <input
+        type="button"
+        className="btn btn-primary"
+        value="PRIMARY"
+        onClick={changeBackgroundPrimary}
+      />
+      <input
+        type="button"
+        className="btn btn-success"
+        value="SUCCESS"
+        onClick={changeBackgroundSuccess}
+      />
+      <input
+        type="button"
+        className="btn btn-danger"
+        value="DANGER"
+        onClick={changeBackgroundDanger}
+      />
     </div>
   );
 }
