54eebc3f26f59fe2483bd54f6d162c80fe5f602f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 12:10:19 +0530

    working with list,

--- src/App.js
+++ src/App.js
@@ -8,41 +8,26 @@ function App() {
   );
 }
 
+// MAP OPERATOR
+// <h1>Delhi</h1>
 function EventDemo() {
-  // let counter = 100;
-  let [counter, setCounter] = useState(100);
-  let [displayImage, setDisplayImage] = useState(
-    "https://picsum.photos/id/237/300/300"
-  );
-
-  let likeMe = () => {
-    counter = counter + 1;
-
-    // PERFORM DOM OPERATION
-    setCounter(counter);
-  };
-
-  let changeImage = () => {
-    displayImage = "https://picsum.photos/id/238/300/300";
-    // perform dom operation
-    setDisplayImage(displayImage);
-  };
-
-  let resetImage = () => {
-    displayImage = "https://picsum.photos/id/237/300/300";
-    // perform dom operation
-    setDisplayImage(displayImage);
-  };
+  let list = ["Delhi", "Calcutta", "Kochi", "Mumbai"];
 
   return (
     <div>
-      <img src={displayImage} />
-      <h1>Like {counter}</h1>
+      <h1>List Demo </h1>
 
-      <input type="button" value="Like Me" onClick={likeMe} />
-      <br />
-      <input type="button" value="Chnage Image" onClick={changeImage} />
-      <input type="button" value="Reset Image" onClick={resetImage} />
+      {list.map((item) => (
+        <div className="alert alert-primary mb-1">{item}</div>
+      ))}
+
+      {/** ANY AS HEADLINE */}
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
+
+      {/** SIMPLE STING */}
+      {list.map((item) => item)}
     </div>
   );
 }
