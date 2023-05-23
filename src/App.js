e11886439901b7b0f192d7b15634994162be0370
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-23 11:07:17 +0530

    basic setup and bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EventDemo />
+    </div>
+  );
+}
+
+function EventDemo() {
+  return (
+    <div>
+      <h1 className="bg-primary text-light p-3">Event DEMO</h1>
     </div>
   );
 }
